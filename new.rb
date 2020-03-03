26f7ab3ac344f7af9ea4cb0d93d65ec19107ba75
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-03 22:03:14 +0000

    Done.

--- new.rb
+++ new.rb
@@ -1,5 +1,54 @@
-def happy_birthday(name: "hi" ,  aa: "yo")
-  puts "#{name}   #{aa}"
+#require 'pry'
+require 'nokogiri'
+require 'open-uri'
+times = 0
+all_pokemon = []
  
+ 
+doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_generation_I_Pok%C3%A9mon"))
+
+
+  out = true
+  
+doc.css('th').each do |item|
+  pokemon = item.text
+ 
+if out == false && pokemon.chomp != "vtePokémon species"
+ puts pokemon
+ 
+   all_pokemon << pokemon
+ 
+ end
+ if pokemon.chomp == "Secondary"
+   out = false
+   elsif pokemon.chomp == "vtePokémon species"
+   out = true
 end
-happy_birthday( name: "1")
+
+end
+
+puts "Enter pokemon"
+pokemon_search = gets
+if pokemon_search.chomp == "Mew"
+  pokemon_search = "Mew_(Pok%C3%A9mon)"
+end
+wiki_doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/"+ pokemon_search.chomp))
+
+
+wiki_doc.css('p').each do |peragraph|
+  
+  info = peragraph.text
+  if times < 2
+  puts info
+end
+  times += 1
+end
+
+
+times = 0
+#film = gets
+#ary.each do |show|
+#  if show 
+#end
+
+#ruby new.rb
