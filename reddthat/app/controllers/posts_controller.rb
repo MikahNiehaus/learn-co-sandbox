fe4ac8092791dc7f81f1a92302b0e85283484225
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-10 16:54:55 +0000

    re-did sucurity eith more efficent code

--- reddthat/app/controllers/posts_controller.rb
+++ reddthat/app/controllers/posts_controller.rb
@@ -3,60 +3,38 @@ class PostsController < ApplicationController
 
  
   get '/posts' do
-     if logged_in? 
     @posts = Post.all
     erb :'/posts/index'
-     else
-     redirect to "/failure"
-   end
   end
 
   get '/posts/new' do
-     if logged_in? 
     @topics = Topic.all
-    # binding.pry
     erb :'/posts/new'
-     else
-     redirect to "/failure"
-   end
   end
 
   post '/posts' do
-    if logged_in? 
     @post = Post.create(params[:post])
     if !params["topic"]["text"].empty?
       @post.topic = Topic.create(text: params["topic"]["text"])
     end
     @post.save
     redirect to "posts/#{@post.id}"
-      else
-     redirect to "/failure"
-   end
   end
 
   get '/posts/:id' do
-     if logged_in? 
     @post = Post.find(params[:id])
         @topics = Topic.all
     erb :'/posts/show'
-        else
-     redirect to "/failure"
-   end
   end
 
-get '/posts/:id/edit' do
-   if logged_in? 
+  get '/posts/:id/edit' do
     @post = Post.find_by_id(params[:id])
     @topics = Topic.all
     erb :'/posts/edit'
-            else
-     redirect to "/failure"
-   end
   end
 
 
   post '/posts/:id' do
-      if logged_in? 
     @post = Post.find(params[:id])
     @post.update(params["post"])
     if !params["topic"]["text"].empty?
@@ -64,8 +42,7 @@ get '/posts/:id/edit' do
     end
     @post.save
     redirect to "posts/#{@post.id}"
-                else
-     redirect to "/failure"
-   end
   end
+  
+  
 end
--- reddthat/app/controllers/topics_controller.rb
+++ reddthat/app/controllers/topics_controller.rb
@@ -2,71 +2,43 @@ require 'pry'
 class TopicsController < ApplicationController
 
   get '/topics' do
-   
-       if logged_in? 
    @topics = Topic.all
-    erb :'/topics/index'
-  else
-     redirect to "/failure"
-   end
+   erb :'/topics/index'
   end 
 
   get '/topics/new' do
-    # binding.pry
-    if logged_in? 
     @posts = Post.all
     erb :'/topics/new'
-  else
-     redirect to "/failure"
-   end
   end
 
 
   post '/topics' do
-     if logged_in? 
-      # binding.pry
     @topic = Topic.create(params[:topic])
-    
     if !params["post"]["text"].empty?
       @topic.posts << Post.create(text: params["post"]["text"])
     end
     @topic.save
     redirect "topics/#{@topic.id}"
-     else
-     redirect to "/failure"
-   end
   end
 
   get '/topics/:id/edit' do
-     if logged_in? 
     @topic = Topic.find(params[:id])
-    # binding.pry
     erb :'/topics/edit'
-       else
-     redirect to "/failure"
-   end
   end
 
   get '/topics/:id' do
-      if logged_in? 
-        # binding.pryp
     @topic = Topic.find(params[:id])
     erb :'/topics/show'
-       else
-     redirect to "/failure"
-   end
   end
 
   post '/topics/:id' do
-      if logged_in? 
     @topic = Topic.find(params[:id])
     @topic.update(params["topic"])
     if !params["post"]["text"].empty?
       @topic.posts << Post.create(text: params["post"]["text"])
     end
     redirect to "topics/#{@topic.id}"
-      else
-     redirect to "/failure"
-   end
   end
+  
+  
 end
